60301ca75afa85a3cab5b0b4349b3077e240f231
tikodak <samueltiko55@gmail.com>
2024-03-22 14:42:28 +0300

    second commit

--- daraja-tutorial-master/stk_initiate.php
+++ daraja-tutorial-master/stk_initiate.php
@@ -5,8 +5,8 @@ if(isset($_POST['submit'])){
   date_default_timezone_set('Africa/Nairobi');
 
   # access token
-  $consumerKey = 'nk16Y74eSbTaGQgc9WF8j6FigApqOMWr'; //Fill with your app Consumer Key
-  $consumerSecret = '40fD1vRXCq90XFaU'; // Fill with your app Secret
+  $consumerKey = 'XjKdTmp12MNAHGohtBVbTbeGPCN9yZclgXswzteZBnVZkn4I'; //Fill with your app Consumer Key
+  $consumerSecret = 'HdTnq4vsvHjnGd54Ki9t4NjwrtQoGh1hj7fI9ptKnQvQrGQxILrbx2HnZS42rOaW'; // Fill with your app Secret
 
   # define the variales
   # provide the following details, this part is found on your test credentials on the developer account
@@ -42,7 +42,7 @@ if(isset($_POST['submit'])){
   $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
 
   # callback url
-  $CallBackURL = 'https://morning-basin-87523.herokuapp.com/callback_url.php';  
+  $CallBackURL = 'https://dry-reef-86308-950b0ed103b9.herokuapp.com//callback_url.php';  
 
   $curl = curl_init($access_token_url);
   curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
